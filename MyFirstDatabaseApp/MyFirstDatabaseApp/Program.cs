4e9fdf46d5df925be722009b40962670a176765a
Ravi Makwana <ravi.makwana@tarktech.com>
2020-11-22 16:28:46 +0530

    ADO.NET Programs

--- MyFirstDatabaseApp/MyFirstDatabaseApp/Program.cs
+++ MyFirstDatabaseApp/MyFirstDatabaseApp/Program.cs
@@ -5,33 +5,35 @@ using System.IO;
 
 namespace MyFirstDatabaseApp
 {
-    public class Customer
-    {
-        public int Id;
-        public string Name;
-
-        public Customer() { }
-        public Customer(int id, string name)
-        {
-            Id = id;
-            Name = name;
-        }
-        public void Show()
-        {
-            Console.WriteLine("Id : "+Id+" , Name : "+Name);
-        }
-    }
     class Program
     {
         static void Main(string[] args)
         {
+
             SqlConnection con = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=G:\\C# Programming\\MyFirstDatabaseApp\\MyFirstDatabaseApp\\Employee.mdf;Integrated Security=True");
-            string sql = "Update Customer set name = @name where id= @id";
+            string sql = "INSERT INTO emp VALUES (@empno, @ename, @job, @mgr, @hiredate, @sal, @comm, @deptno)";
             con.Open();
-            SqlCommand cmd = new SqlCommand(sql, con);
-            cmd.Parameters.Add(new SqlParameter("@name","Gujart Technical University"));
-            cmd.Parameters.Add(new SqlParameter("@id", 3));
-            cmd.ExecuteNonQuery();
+            
+            StreamReader reader = new StreamReader("EmployeeData.csv");
+            reader.ReadLine();
+            string line;
+            while((line=reader.ReadLine())!=null)
+            {
+                var part = line.Split(',');
+                SqlCommand cmd = new SqlCommand(sql, con);
+                cmd.Parameters.Add(new SqlParameter("@empno", int.Parse(part[0])));
+                cmd.Parameters.Add(new SqlParameter("@ename", part[1]));
+                cmd.Parameters.Add(new SqlParameter("@job", part[2]));
+                cmd.Parameters.Add(new SqlParameter("@mgr", int.Parse(part[3])));
+                cmd.Parameters.Add(new SqlParameter("@hiredate", DateTime.Parse(part[4])));
+                cmd.Parameters.Add(new SqlParameter("@sal", int.Parse(part[5])));
+                cmd.Parameters.Add(new SqlParameter("@comm", int.Parse(part[6])));
+                cmd.Parameters.Add(new SqlParameter("@deptno", int.Parse(part[7])));
+
+                cmd.ExecuteNonQuery();
+            }
+
+            reader.Close();
             con.Close();
             con.Dispose();
             Console.ReadKey();
